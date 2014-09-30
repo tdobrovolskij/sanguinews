1666b679a41ef52db79ff735560e8f02303e867d
Tadeus Dobrovolskij <admin@tad-do.net>
2014-09-30 07:08:33 +0200

    Updated version info.

--- sanguinews.rb
+++ sanguinews.rb
@@ -17,7 +17,7 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 ########################################################################
 
-@version = '0.56.1'
+@version = '0.57'
 
 require 'rubygems'
 require 'bundler/setup'
